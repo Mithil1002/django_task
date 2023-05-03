80728706338390aaa6dac6f3eb13ed143d650996
Mithil <mithilhingrajiya10@gmail.com>
2023-05-03 11:00:36 +0530

    all

--- excercise/excercise/settings.py
+++ excercise/excercise/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = 'django-insecure-nu=lhy*nmj9o=o6*a#45h@#ijhz_-c7&qs@l=*6l!mu@^xli!!'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
